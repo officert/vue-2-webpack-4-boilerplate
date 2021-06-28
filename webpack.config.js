1c2b47d96606b2bf62a60daef615722cbcd87078
Tim Officer <timothyofficer@gmail.com>
2021-06-28 09:54:47 -0400

    format

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-'use strict';
+'use strict'
 
-const environment = (process.env.NODE_ENV || 'development').trim();
+const environment = (process.env.NODE_ENV || 'development').trim()
 
 if (environment === 'development') {
-    module.exports = require('./config/webpack.config.dev');
+  module.exports = require('./config/webpack.config.dev')
 } else {
-    module.exports = require('./config/webpack.config.prod');
+  module.exports = require('./config/webpack.config.prod')
 }
